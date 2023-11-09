118ff9d05a5ccd14fd3fb3332903e4bc3990fe45
Sameer Sannidhanam <110780161+sameersannidhanam@users.noreply.github.com>
2023-11-09 22:40:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AgeCalculator from "./components/AgeCalculator";
+
+function App() {
+  return (
+    <div>
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
